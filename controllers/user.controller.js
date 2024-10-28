406a97db724f37b08403ab6644a12875268da9f2
1253William <williamofosuparwar@gmsil.com>
2024-10-28 09:34:11 +0000

    Updated User-controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,8 @@
 const User = require('../models/user.model');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+require('dotenv').config();
+
 
 
 /// Controller for Registering a new user
@@ -12,6 +14,7 @@ exports.register = async(req, res)=>{
         if(existingUser){
             return res.status(400).json({message: "User already exists"});
         }
+       
         const salt = await bcrypt.genSalt(10); 
         const hashedPassword = await bcrypt.hash(password, salt);
         const user = new User({
@@ -19,6 +22,7 @@ exports.register = async(req, res)=>{
             email,
             password: hashedPassword
         })
+
         const savedUser = await user.save();
         return res.status(201).json({
             message: "User registered successfully",
@@ -34,6 +38,17 @@ exports.register = async(req, res)=>{
 /// Controller for Logging in an existing user
 ///POST /api/v1/auth/login
 exports.login = async(req, res) => {
+    try {
+
+    } catch(error){
+        return res.status(500).json({message: error.message});
+    }
+}
+
+
+/// Controller for resetting password
+/// POST /api/v1/auth/resetpassword
+exports.resetPassword = async(req, res) => {
     try {
 
     } catch(error){
